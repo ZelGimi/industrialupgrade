e579c0163672dafc8f1f763f6aaf77f15877444d
ijo42 <admin@ijo42.ru>
2020-12-18 22:36:20 +0500

    Perfect drill optimize

--- src/main/java/com/denfop/ssp/items/tools/ItemUltDrill.java
+++ src/main/java/com/denfop/ssp/items/tools/ItemUltDrill.java
@@ -49,37 +49,6 @@ public class ItemUltDrill extends ItemDrill {
 		BlocksItems.registerItem((Item) this, SuperSolarPanels.getIdentifier(NAME)).setUnlocalizedName(NAME);
 	}
 
-	@Nonnull
-	public static Collection<BlockPos> getBrokenBlocks(EntityPlayer player, RayTraceResult ray) {
-		return getBrokenBlocks(player, ray.getBlockPos(), ray.sideHit);
-	}
-
-	protected static Collection<BlockPos> getBrokenBlocks(EntityPlayer player, BlockPos pos, EnumFacing side) {
-		int xMove = 1, yMove = 1, zMove = 1;
-		switch (side.getAxis()) {
-			case X:
-				xMove = 0;
-				break;
-			case Y:
-				yMove = 0;
-				break;
-			case Z:
-				zMove = 0;
-				break;
-		}
-		Collection<BlockPos> list = new ArrayList<>(9);
-		for (int x = pos.getX() - xMove; x <= pos.getX() + xMove; x++) {
-			for (int y = pos.getY() - yMove; y <= pos.getY() + yMove; y++) {
-				for (int z = pos.getZ() - zMove; z <= pos.getZ() + zMove; z++) {
-					BlockPos potential = new BlockPos(x, y, z);
-					if (canBlockBeMined(player.world, potential, player, false))
-						list.add(potential);
-				}
-			}
-		}
-		return list;
-	}
-
 	protected static boolean canBlockBeMined(World world, BlockPos pos, EntityPlayer player, boolean skipEffectivity) {
 		IBlockState state = world.getBlockState(pos);
 		return (state.getBlock().canHarvestBlock(world, pos, player) && (skipEffectivity || isEffective(state.getMaterial())) && state.getPlayerRelativeBlockHardness(player, world, pos) != 0.0F);
@@ -95,97 +64,52 @@ public class ItemUltDrill extends ItemDrill {
 
 	public boolean onBlockStartBreak(@Nonnull ItemStack stack, @Nonnull BlockPos pos, @Nonnull EntityPlayer player) {
 		World world;
-		if (ElectricItem.manager.canUse(stack, this.operationEnergyCost)) {
-			if (readDrillMode(stack) == DrillMode.BIG_HOLES && !(world = player.world).isRemote) {
-				Collection<BlockPos> blocks = getBrokenBlocks(player, rayTrace(world, player, true));
-				if (!blocks.contains(pos) && canBlockBeMined(world, pos, player, true))
+		if (readDrillMode(stack) != DrillMode.NORMAL && !(world = player.world).isRemote) {
+			if (!ElectricItem.manager.canUse(stack, this.operationEnergyCost))
+				return false;
+			Collection<BlockPos> blocks = readDrillMode(stack).getBrokenBlocks(player, rayTrace(world, player, true));
+			if (canBlockBeMined(world, pos, player, true))
+				if (!blocks.contains(pos)) {
 					blocks.add(pos);
-				boolean powerRanOut = false;
-				for (BlockPos blockPos : blocks) {
-					if (!world.isBlockLoaded(blockPos))
-						continue;
-					IBlockState state = world.getBlockState(blockPos);
-					Block block = state.getBlock();
-					if (!block.isAir(state, world, blockPos)) {
-						int experience;
-						if (player instanceof EntityPlayerMP) {
-							experience = ForgeHooks.onBlockBreakEvent(world, ((EntityPlayerMP) player).interactionManager.getGameType(), (EntityPlayerMP) player, blockPos);
-							if (experience < 0)
-								return false;
-						} else
-							experience = 0;
-						block.onBlockHarvested(world, blockPos, state, player);
-						if (player.isCreative()) {
-							if (block.removedByPlayer(state, world, blockPos, player, false))
-								block.onBlockDestroyedByPlayer(world, blockPos, state);
-						} else {
-							if (block.removedByPlayer(state, world, blockPos, player, true)) {
-								block.onBlockDestroyedByPlayer(world, blockPos, state);
-								block.harvestBlock(world, player, blockPos, state, world.getTileEntity(blockPos), stack);
-								if (experience > 0)
-									block.dropXpOnBlockBreak(world, blockPos, experience);
-							}
-							stack.onBlockDestroyed(world, state, blockPos, player);
+				}
+			boolean powerRanOut = false;
+			for (BlockPos blockPos : blocks) {
+				if (!world.isBlockLoaded(blockPos))
+					continue;
+				IBlockState state = world.getBlockState(blockPos);
+				Block block = state.getBlock();
+				if (!block.isAir(state, world, blockPos)) {
+					int experience;
+					if (player instanceof EntityPlayerMP) {
+						experience = ForgeHooks.onBlockBreakEvent(world, ((EntityPlayerMP) player).interactionManager.getGameType(), (EntityPlayerMP) player, blockPos);
+						if (experience < 0)
+							return false;
+					} else
+						experience = 0;
+					block.onBlockHarvested(world, blockPos, state, player);
+					if (player.isCreative()) {
+						if (block.removedByPlayer(state, world, blockPos, player, false))
+							block.onBlockDestroyedByPlayer(world, blockPos, state);
+					} else {
+						if (block.removedByPlayer(state, world, blockPos, player, true)) {
+							block.onBlockDestroyedByPlayer(world, blockPos, state);
+							block.harvestBlock(world, player, blockPos, state, world.getTileEntity(blockPos), stack);
+							if (experience > 0)
+								block.dropXpOnBlockBreak(world, blockPos, experience);
 						}
-						world.playEvent(2001, blockPos, Block.getStateId(state));
-						if (player instanceof EntityPlayerMP) {
-							((EntityPlayerMP) player).connection.sendPacket(new SPacketBlockChange(world, blockPos));
-						}
+						stack.onBlockDestroyed(world, state, blockPos, player);
+					}
+					world.playEvent(2001, blockPos, Block.getStateId(state));
+					if (player instanceof EntityPlayerMP) {
+						((EntityPlayerMP) player).connection.sendPacket(new SPacketBlockChange(world, blockPos));
 					}
 				}
-				if (powerRanOut)
-					IC2.platform.messagePlayer(player, "super_solar_panels.item_ult_drill.ranOut");
-				return true;
 			}
-			if (readDrillMode(stack) == DrillMode.BIG_HOLES1 && !(world = player.world).isRemote) {
-				Collection<BlockPos> blocks = getBrokenBlocks1(player, rayTrace(world, player, true));
-				if (!blocks.contains(pos) && canBlockBeMined(world, pos, player, true))
-					blocks.add(pos);
-				boolean powerRanOut = false;
-				for (BlockPos blockPos : blocks) {
-
-					if (!world.isBlockLoaded(blockPos))
-						continue;
-					IBlockState state = world.getBlockState(blockPos);
-					Block block = state.getBlock();
-					if (!block.isAir(state, world, blockPos)) {
-						int experience;
-						if (player instanceof EntityPlayerMP) {
-							experience = ForgeHooks.onBlockBreakEvent(world, ((EntityPlayerMP) player).interactionManager.getGameType(), (EntityPlayerMP) player, blockPos);
-							if (experience < 0)
-								return false;
-						} else
-							experience = 0;
-						block.onBlockHarvested(world, blockPos, state, player);
-						if (player.isCreative()) {
-							if (block.removedByPlayer(state, world, blockPos, player, false))
-								block.onBlockDestroyedByPlayer(world, blockPos, state);
-						} else {
-							if (block.removedByPlayer(state, world, blockPos, player, true)) {
-								block.onBlockDestroyedByPlayer(world, blockPos, state);
-								block.harvestBlock(world, player, blockPos, state, world.getTileEntity(blockPos), stack);
-								if (experience > 0)
-									block.dropXpOnBlockBreak(world, blockPos, experience);
-							}
-							stack.onBlockDestroyed(world, state, blockPos, player);
-						}
-						world.playEvent(2001, blockPos, Block.getStateId(state));
-						if (player instanceof EntityPlayerMP) {
-							((EntityPlayerMP) player).connection.sendPacket(new SPacketBlockChange(world, blockPos));
-						}
-					}
-				}
-				if (powerRanOut)
-					IC2.platform.messagePlayer(player, "super_solar_panels.item_ult_drill.ranOut");
-				return true;
-			}
+			if (powerRanOut)
+				IC2.platform.messagePlayer(player, "super_solar_panels.item_ult_drill.ranOut");
+			return true;
 		}
 		return super.onBlockStartBreak(stack, pos, player);
-	}
-
-	@Nonnull
-	public static Collection<BlockPos> getBrokenBlocks1(EntityPlayer player, @Nonnull RayTraceResult ray) {
-		return getBrokenBlocks1(player, ray.getBlockPos(), ray.sideHit);
 	}
 
 	public String getUnlocalizedName() {
@@ -234,39 +158,10 @@ public class ItemUltDrill extends ItemDrill {
 		return DrillMode.getFromID(StackUtil.getOrCreateNbtData(stack).getInteger("toolMode"));
 	}
 
-	@Nonnull
-	protected static Collection<BlockPos> getBrokenBlocks1(EntityPlayer player, BlockPos pos, @Nonnull EnumFacing side) {
-		int xMove = 2, yMove = 2, zMove = 2;
-		switch (side.getAxis()) {
-			case X:
-				xMove = 0;
-				break;
-			case Y:
-				yMove = 0;
-				break;
-			case Z:
-				zMove = 0;
-				break;
-		}
-		Collection<BlockPos> list = new ArrayList<>(25);
-		for (int x = pos.getX() - xMove; x <= pos.getX() + xMove; x++) {
-			for (int y = pos.getY() - yMove; y <= pos.getY() + yMove; y++) {
-				for (int z = pos.getZ() - zMove; z <= pos.getZ() + zMove; z++) {
-					BlockPos potential = new BlockPos(x, y, z);
-					if (canBlockBeMined(player.world, potential, player, false))
-						list.add(potential);
-				}
-			}
-		}
-		return list;
-	}
-
 	public enum DrillMode {
-		NORMAL(TextFormatting.DARK_GREEN, Configs.drillSpeed, Configs.energyCost),
-		BIG_HOLES(TextFormatting.AQUA, Configs.drillSpeed1, Configs.energyCost1),
-		BIG_HOLES1(TextFormatting.LIGHT_PURPLE, Configs.drillSpeed2, Configs.energyCost2);
-
-		private static final DrillMode[] VALUES = values();
+		NORMAL(TextFormatting.DARK_GREEN, Configs.drillSpeed, Configs.energyCost, 0),
+		BIG_HOLES(TextFormatting.AQUA, Configs.drillSpeed1, Configs.energyCost1, 1),
+		BIG_HOLES1(TextFormatting.LIGHT_PURPLE, Configs.drillSpeed2, Configs.energyCost2, 2);
 
 		public final float drillSpeed;
 
@@ -276,16 +171,46 @@ public class ItemUltDrill extends ItemDrill {
 
 		public final String translationName;
 
-		DrillMode(TextFormatting colour, float speed, double energyCost) {
+		public final int range;
+
+		DrillMode(TextFormatting colour, float speed, double energyCost, int range) {
 			this.translationName = "super_solar_panels.item_ult_drill.mode." + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name());
 			this.energyCost = energyCost;
 			this.drillSpeed = speed;
 			this.colour = colour;
+			this.range = range;
 		}
 
 		public static DrillMode getFromID(int ID) {
-			return VALUES[ID % VALUES.length];
+			return values()[ID % values().length];
 		}
 
+		@Nonnull
+		private Collection<BlockPos> getBrokenBlocks(EntityPlayer player, @Nonnull RayTraceResult ray) {
+			int xMove = this.range, yMove = this.range, zMove = this.range;
+			EnumFacing side = ray.sideHit;
+			BlockPos pos = ray.getBlockPos();
+			switch (side.getAxis()) {
+				case X:
+					xMove = 0;
+					break;
+				case Y:
+					yMove = 0;
+					break;
+				case Z:
+					zMove = 0;
+					break;
+			}
+
+			Collection<BlockPos> list = new ArrayList<>(1 + 2 * this.range ^ 2);
+			for (int x = pos.getX() - xMove; x <= pos.getX() + xMove; x++)
+				for (int y = pos.getY() - yMove; y <= pos.getY() + yMove; y++)
+					for (int z = pos.getZ() - zMove; z <= pos.getZ() + zMove; z++) {
+						BlockPos potential = new BlockPos(x, y, z);
+						if (canBlockBeMined(player.world, potential, player, false))
+							list.add(potential);
+					}
+			return list;
+		}
 	}
 }
